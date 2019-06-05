39657b6822ff5750ce1f6701d81c408b1f088780
pandasanjay <panda.sanjay18@gmail.com>
2019-06-05 20:35:09 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4dd4e7c47f8b5433136e.js"
+    "url": "webpack-runtime-406e128b26907c0571d6.js"
   },
   {
     "url": "styles.24b9d7ed9d23a0c32510.css"
@@ -42,13 +42,13 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a43ad7aaf218927766ddd1aa352c7926"
+    "revision": "4ff6c0a30c124897a5f00e16b736d38c"
   },
   {
     "url": "0-042257278153770ec13c.js"
   },
   {
-    "url": "component---src-pages-404-js-996effaaa4e65a3b03e0.js"
+    "url": "component---src-pages-404-js-27bee3a579c562c09440.js"
   },
   {
     "url": "static/d/285/path---404-html-516-62a-0SUcWyAf8ecbYDsMhQkEfPzV8.json"
