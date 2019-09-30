b0718c76475ab2ecb6be1e3bb2048e8913380f90
Sanjay <panda.sanjay18@gmail.com>
2019-10-01 00:23:02 +0100

    Updates

---
+++ styles-4a4275f954bb36ecef5a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{171:function(n,w,o){}}]);
+//# sourceMappingURL=styles-4a4275f954bb36ecef5a.js.map
--- sw.js
+++ sw.js
@@ -26,37 +26,37 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-89be08e4d8349710f834.js"
+    "url": "webpack-runtime-d4fdc3bacc28a5383577.js"
   },
   {
-    "url": "commons-09ebd4dba76c42dfc6b4.js"
+    "url": "commons-d3660dc20d48030f98a5.js"
   },
   {
-    "url": "styles.24b9d7ed9d23a0c32510.css"
+    "url": "styles.6021e032ea68f6eae144.css"
   },
   {
-    "url": "styles-d67b6ca73dd1dc6d811d.js"
+    "url": "styles-4a4275f954bb36ecef5a.js"
   },
   {
-    "url": "app-31e1eaeac19b6a98aebf.js"
+    "url": "app-c587bbb5b2583e4baf4e.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-f349341ff30029d83e9e.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "02c881136472cf97121faf5b1426fe2a"
+    "revision": "afac11122985ca4d68ad243b2662eedc"
   },
   {
-    "url": "component---src-pages-404-js-9b45cbc769d690ac09bf.js"
+    "url": "component---src-pages-404-js-608b414162d6907a8823.js"
   },
   {
     "url": "page-data/404.html/page-data.json",
-    "revision": "83bf2e8afcf625d3df58a9be32278e7c"
+    "revision": "c7d4c597d892df3d318efa00496df3fe"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "d9b467191b2b21719acbb94fe1f14742"
+    "revision": "798632eed4b45db3e98ebc65847076b9"
   },
   {
     "url": "manifest.webmanifest",
